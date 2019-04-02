21a80be4cf384d4fd1312da38b48045a700c428d
alexsh97 <alexsh997@gmail.com>
2019-04-03 00:57:43 +0200

    Updated
    Metody klasy SingleResponsibilityPrincipleManager teraz static.

--- src/main/java/edu/agh/wfiis/solid/srp/example2/SingleResponsibilityPrincipleManager.java
+++ src/main/java/edu/agh/wfiis/solid/srp/example2/SingleResponsibilityPrincipleManager.java
@@ -3,21 +3,21 @@ import java.io.File;
 
 public class SingleResponsibilityPrincipleManager {
 
-    public String getDescription() {
+    public static String getDescription() {
         return "SRP description";
     }
 
-    public String refactorCode(String badCode) {
+    public static String refactorCode(String badCode) {
         /* some magic happens here, irrelevant from this example perspective...*/
         return "code refactored using SRP";
     }
 
-    public String extractBadCodeFromFile(File file) {
+    public static String extractBadCodeFromFile(File file) {
         /* some magic happens here, irrelevant from this example perspective...*/
         return "bad code from file";
     }
 
-    public String extractBadCodeFromClassPath(ClassLoader classLoader) {
+    public static String extractBadCodeFromClassPath(ClassLoader classLoader) {
         /* some magic happens here, irrelevant from this example perspective...*/
         return "bad code from class loader";
     }

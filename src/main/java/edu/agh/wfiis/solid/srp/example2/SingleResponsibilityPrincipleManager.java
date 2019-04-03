5d3a11d17830fb819019c4169a5e44374838a9de
gravvy23 <majkutw@gmail.com>
2019-04-03 20:37:06 +0200

    fix(SingleResposibilityPrincipleManager): refactoring

--- src/main/java/edu/agh/wfiis/solid/srp/example2/SingleResponsibilityPrincipleManager.java
+++ src/main/java/edu/agh/wfiis/solid/srp/example2/SingleResponsibilityPrincipleManager.java
@@ -1,26 +1,69 @@
 package edu.agh.wfiis.solid.srp.example2;
+
 import java.io.File;
 
-public class SingleResponsibilityPrincipleManager {
+public class SRPConverter {
+    private String description;
+    
+    private String codeToBeRefactored;
 
-    public String provideDescription() {
-        return "SRP description";
+    private SRPConverter(String description){
+        this.description = description;
     }
 
-    public String refactorCode(String badCode) {
+    public static SRPConverter withDefaultDescription(){
+        return new SRPConverter("SRP description");
+    }
+
+    public static SRPConverter withProvidedDescription(String description){
+        return new SRPConverter(description);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    private String refactorCode(){
         /* some magic happens here, irrelevant from this example perspective...*/
         return "code refactored using SRP";
     }
 
-    public String extractBadCodeFromFile(File file) {
-        /* some magic happens here, irrelevant from this example perspective...*/
-        return "bad code from file";
+    public String refactorCodeFromFile(File file) {
+        try {
+            this.codeToBeRefactored = SRPCodeExtractor.extractFromFile(file);
+        }
+        catch (Exception e){
+            /* some magic happens here */
+        }
+        return refactorCode();        
     }
 
-    public String extractBadCodeFromClassPath(ClassLoader classLoader) {
+    public String refactorCodeFromClassPath(ClassLoader classLoader) {
+        try {
+            this.codeToBeRefactored = SRPCodeExtractor.extractFromClassPath(classLoader);
+        }
+        catch (Exception e){
+            /* some magic happens here */
+        }
+        return refactorCode();        
+    }
+}
+
+class SRPCodeExtractor {
+
+    public static string extractFromFile(File file) throws Exception {
+        /* some magic happens here, irrelevant from this example perspective...*/
+        return "bad code from file"; 
+    }
+
+    public static string extractFromClassPath(Class classLoader) throws Exception {
         /* some magic happens here, irrelevant from this example perspective...*/
         return "bad code from class loader";
     }
+}
+
+
+class CourseworkGenerator() {
 
     public String generateCourseworkForStudents() {
         /* some magic happens here, irrelevant from this example perspective...*/

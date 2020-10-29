6a6e5f2938058a14e8816c76bf844e45ee9a8619
Tomasz Pedziwiatr <tpedziwiatr@worldremit.com>
2020-10-29 19:29:49 +0100

    29.10 changes

--- src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
+++ src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
@@ -6,9 +6,10 @@ import edu.agh.wfiis.solid.srp.example1.model.InvalidHeaderException;
 import edu.agh.wfiis.solid.srp.example1.model.MuleMessage;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpRestRequest {
-
     protected MuleMessage muleMessage;
     protected Constraints validationConstraints;
 
@@ -18,28 +19,85 @@ public class HttpRestRequest {
 
     public MuleMessage validate(Constraints validationConstraints) throws InvalidHeaderException {
         this.validationConstraints = validationConstraints;
-        processHeaders();
+
+        List<HeaderValidationError> headerValidationErrors = validateHeaders(validationConstraints.getHeaderConstraints());
+
+        if(!headerValidationErrors.isEmpty()) {
+            throw new InvalidHeaderException(headerValidationErrors.get(0).getError());
+        }
+
+        setMissingHeadersDefaultValues();
         return muleMessage;
     }
 
-    private void processHeaders() throws InvalidHeaderException {
-        for (Constraint constraint : validationConstraints.getHeaderConstraints()) {
+    private List<HeaderValidationError> validateHeaders(Map<String, String> headerValuesByNames, List<Constraint> headerConstraints) {
+        List<HeaderValidationError> errorMessages = new ArrayList<>();
+
+        for (Constraint constraint : headerConstraints) {
             String headerName = constraint.getHeaderName();
             String headerValue = muleMessage.getHeader(headerName);
 
             if (headerValue == null && constraint.isHeaderRequired()) {
-                throw new InvalidHeaderException("Required header " + headerName + " not specified");
-            }
-
-            if (headerValue == null && constraint.getDefaultValue() != null) {
-                muleMessage.setHeader(headerName, constraint.getDefaultValue());
+                errorMessages.add(new MissingHeaderValidationError(headerName));
             }
 
             if (headerValue != null) {
                 if (!constraint.validate(headerValue)) {
-                    throw new InvalidHeaderException(MessageFormat.format("Invalid value format for header {0}.", headerName));
+                    errorMessages.add(new InvalidHeaderValueValidationError(headerName, headerValue));
                 }
             }
         }
+
+        return errorMessages;
+    }
+
+    private void setMissingHeadersDefaultValuesInMessage( ){
+        for (Constraint constraint : validationConstraints.getHeaderConstraints()) {
+            tryToSetDefaultHeaderValue(constraint,muleMessage);
+        }
+    }
+
+    private boolean tryToSetDefaultHeaderValue(Constraint constraint, MuleMessage muleMessage){
+        if (muleMessage.getHeader(constraint.getHeaderName()) == null && constraint.getDefaultValue() != null) {
+            setDefaultHeaderValue(constraint, muleMessage);
+            return true;
+        }
+        return false;
+    }
+
+    private void setDefaultHeaderValue(Constraint constraint, MuleMessage muleMessage){
+        muleMessage.setHeader(constraint.getHeaderName(), constraint.getDefaultValue());
+    }
+}
+
+interface HeaderValidationError {
+    String getError();
+}
+
+class MissingHeaderValidationError  implements HeaderValidationError {
+    private final String headerName;
+
+    public MissingHeaderValidationError(String headerName) {
+        this.headerName = headerName;
+    }
+
+    @Override
+    public String getError() {
+        return "Required header " + headerName + " not specified";
+    }
+}
+
+class InvalidHeaderValueValidationError  implements HeaderValidationError {
+    private final String headerName;
+    private final String headerValue;
+
+    public InvalidHeaderValueValidationError(String headerName, String headerValue) {
+        this.headerName = headerName;
+        this.headerValue = headerValue;
+    }
+
+    @Override
+    public String getError() {
+        return MessageFormat.format("Invalid value {0} format for header {1}.", headerValue, headerName);
     }
 }
